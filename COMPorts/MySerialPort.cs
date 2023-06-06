85e1c6f25a0722fcff347a799322dbe997a1a587
artemka1511 <artemka15111992@gmail.com>
2023-06-06 15:38:58 +0700

    first commit

---
+++ COMPorts/MySerialPort.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMPorts
+{
+    class MySerialPort : SerialPort
+    {
+        private const int DataSize = 54;
+        private readonly byte[] _bufer = new byte[DataSize];
+        private int _stepIndex;
+        private bool _startRead;
+        private bool _virtualPort;
+
+        public MySerialPort(string port, bool virtualPort) : base()
+        {
+            PortName = port;
+            BaudRate = 115200;
+            DataBits = 8;
+            StopBits = StopBits.Two;
+            Parity = Parity.None;
+            ReadTimeout = 1000;
+            _virtualPort = virtualPort;
+
+            //DataReceived += SerialPort_DataReceived;
+        }
+
+        //void SerialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        //{
+        //    var port = (SerialPort)sender;
+        //    try
+        //    {
+        //        byte[] bytes = new byte[256];
+        //        var stream = port.BaseStream;
+
+        //        int i;
+
+        //        while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
+        //        {
+        //            string data = Encoding.ASCII.GetString(bytes, 0, i);
+        //            Console.WriteLine($"Получено сообщение в {PortName}: {data}");
+        //            MessageHandler(data);
+        //        }
+        //    }
+        //    catch(Exception ex)
+        //    {
+        //        Console.WriteLine(ex.Message);
+        //    }
+        //}
+
+        //void MessageHandler(string data)
+        //{
+        //    switch (data)
+        //    {
+        //        case "log version"
+        //        default:
+        //            break;
+        //    }
+        //}
+
+
+    }
+}
---
+++ COMPorts/Program.cs
@@ -0,0 +1,110 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace COMPorts
+{
+    class Program
+    {
+        public static SerialPort VirtualSerialPort { get; set; }
+        public static SerialPort GPSCOMPort { get; set; }
+        public static async Task Main(string[] args)
+        {
+            
+            while (true)
+            {
+                string virtualPortValue = System.Configuration.ConfigurationManager.AppSettings["VirtualCOMPort"];
+
+                string gpsPortValue = System.Configuration.ConfigurationManager.AppSettings["GpsCOMPort"];
+
+                if (!CheckPort(virtualPortValue)) { ShowWarningMessage(virtualPortValue); }
+                else if (!CheckPort(gpsPortValue)) { ShowWarningMessage(virtualPortValue); }
+                else
+                {
+                    CreateAndOpenComPort(virtualPortValue, true);
+                    CreateAndOpenComPort(gpsPortValue, false);
+                }
+
+                Console.ReadLine();
+            }
+        }
+
+        public static void CreateAndOpenComPort(string portName, bool virtualPort)
+        {
+            if(virtualPort)
+            {
+                VirtualSerialPort = new MySerialPort(portName, virtualPort);
+
+                VirtualSerialPort.DataReceived += VirtualSerialPort_DataReceived;
+
+                VirtualSerialPort.Open();
+
+                Console.WriteLine($"Связь с виртуальным COM портом {portName} успешно установлена");
+            }
+            else
+            {
+                GPSCOMPort = new MySerialPort(portName, virtualPort);
+
+                GPSCOMPort.Open();
+
+                Console.WriteLine($"Связь с GPS COM портом {portName} успешно установлена");
+            }
+        }
+
+        public static void VirtualSerialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            var port = (SerialPort)sender;
+            try
+            {
+                byte[] bytes = new byte[256];
+                var stream = port.BaseStream;
+
+                int i;
+
+                while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
+                {
+                    string data = Encoding.ASCII.GetString(bytes, 0, i);
+                    Console.WriteLine($"Получено сообщение в {port.PortName}: {data}");
+                    MessageHandler(data);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        public static void MessageHandler(string data)
+        {
+            switch (data.Replace("\r\n", string.Empty))
+            {
+                case "log version":
+                    VirtualSerialPort.Write("Hello world!");
+                    break;
+                default:
+                    GPSCOMPort.Write(data);
+                    break;
+            }
+        }
+
+        public static void ShowWarningMessage(string portName)
+        {
+            Console.WriteLine($"Ошибка: порт {portName} отсутствует!");
+            Console.ReadLine();
+        }
+
+        public static bool CheckPort(string portName)
+        {
+            string[] ports = SerialPort.GetPortNames();
+
+            if(ports.Contains(portName)) { return true; }
+            else { return false; }
+        }
+    }
+}
